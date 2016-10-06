5e4526b8ec21fdb1cc5d7921069acd77c1a8aecb
Eric Quitevis <ericquitevis@Erics-MacBook-Air.local>
2016-10-06 13:10:07 -0700

    new commit

---
+++ testopen.py
@@ -0,0 +1,11 @@
+import webbrowser
+url1 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Cattempt?conditions=[]'
+url2 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Ccompany?conditions=[]'
+url3 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Ctest?conditions=[]'
+url4 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Cuser?conditions=[]'
+url5 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Cuser_team_mapping?conditions=[]'
+webbrowser.open_new(url1)
+webbrowser.open_new(url2)
+webbrowser.open_new(url3)
+webbrowser.open_new(url4)
+webbrowser.open_new(url5)
