3e669fbe3afd8edd4d18c827e4ee5dd680f43b97
monez <semona.igama@gmail.com>
2017-01-25 07:57:40 -0800

    alterations

--- testopen.py
+++ testopen.py
@@ -1,9 +1,9 @@
 import webbrowser
-url1 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Cattempt?conditions=[]'
+url1 = 'https://staging.hackerrank.net/x/admin/user/merge_accounts'
 url2 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Ccompany?conditions=[]'
-url3 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Ctest?conditions=[]'
+url3 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Ccompany?conditions=[]'
 url4 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Cuser?conditions=[]'
-url5 = 'https://staging.hackerrank.net/x/admin/search/recruit%7Cuser_team_mapping?conditions=[]'
+url5 = 'https://staging.hackerrank.net/x/analytics/companies'
 webbrowser.open_new(url1)
 webbrowser.open_new(url2)
 webbrowser.open_new(url3)
